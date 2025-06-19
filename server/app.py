0b59cf4bcaa34e55519f5b553167baa6a2d65ab7
Chadrick <chadrickalvarez@gmail.com>
2025-06-19 14:27:57 -0400

    lab

--- server/app.py
+++ server/app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_migrate import Migrate
-
 from models import db
 
 app = Flask(__name__)
@@ -11,11 +10,9 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-
 @app.route('/')
 def index():
     return '<h1>Flask SQLAlchemy Lab 2</h1>'
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -17,6 +17,10 @@ class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
+    # Relationships
+    reviews = db.relationship('Review', back_populates='customer', cascade='all, delete-orphan')
+    items = association_proxy('reviews', 'item')
+
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
 
@@ -28,5 +32,43 @@ class Item(db.Model):
     name = db.Column(db.String)
     price = db.Column(db.Float)
 
+    # Relationships
+    reviews = db.relationship('Review', back_populates='item', cascade='all, delete-orphan')
+
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
+
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+
+    customer = db.relationship('Customer', back_populates='reviews')
+    item = db.relationship('Item', back_populates='reviews')
+
+    def __repr__(self):
+        return f'<Review {self.id}, comment="{self.comment}">'
+
+
+# Marshmallow Schemas for serialization
+
+class CustomerSchema(Schema):
+    id = fields.Int()
+    name = fields.Str()
+    # exclude items and reviews to avoid recursion
+
+class ItemSchema(Schema):
+    id = fields.Int()
+    name = fields.Str()
+    price = fields.Float()
+    # exclude reviews and customers to avoid recursion
+
+class ReviewSchema(Schema):
+    id = fields.Int()
+    comment = fields.Str()
+
